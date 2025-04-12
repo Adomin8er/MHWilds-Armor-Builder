145f60a9129c128ab31d012fc2d4d2f2ce8e0b15
Adomin8er <Adam.Holderness@Halosight.com>
2025-04-12 01:28:43 -0600

    Added title bar. Need to figure out spacing for titles.

--- mhwilds-armor-builder/src/App.vue
+++ mhwilds-armor-builder/src/App.vue
@@ -1,7 +1,42 @@
 <template>
   <v-app>
+    <div
+      class="d-flex align-center"
+    >
+      <v-col cols="3">
+        <v-img
+          class="appVue-vImg-logo ml-2"
+          height="45"
+          width="45"
+          :src="Logo">
+        </v-img>
+      </v-col>
+      <v-col cols="6" class="text-h4 text-center">
+        <v-img
+          height="45"
+          :src="Title"
+        >
+        </v-img>
+      </v-col>
+      <v-col cols="3" class="text-end">
+        <Icon_ArtifactLookup
+          class="mr-2 mt-2"
+          height="36"
+          width="36"
+        >
+        </Icon_ArtifactLookup>
+      </v-col>
+    </div>
+    <v-slide-x-reverse-transition>
+      <v-fab
+        v-if="showFab"
+        class="appVue-vSlideXTransitionReverse-showFab"
+        color="primary"
+        :icon="equipmentCardImages.icon_artifactLookup"
+      ></v-fab>
+    </v-slide-x-reverse-transition>
+    <v-divider :thickness="1" class="border-opacity-100"></v-divider>
     <v-main>
-      <v-img class="appVue-vImg-logo" :src="Logo"/>
       <v-container class="appVue-vContainer">
         <v-row
           class="appVue-vRow"
@@ -29,26 +64,37 @@
         </v-row>
       </v-container>
     </v-main>
-    <v-footer class="text-center d-flex flex-column mt-4 py-4" color="indigo-lighten-1">
-      <v-divider class="my-2" thickness="2" width="50"></v-divider>
-      <div class="text-caption font-weight-regular opacity-60">
-        Text
-      </div>
-      <v-divider></v-divider>
-      <div>
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </div>
-    </v-footer>
+    <v-footer class="text-center d-flex flex-column mt-4 py-4"/>
   </v-app>
 </template>
 
 <script setup>
-import Logo from './assets/images/Image_MHWildsArmorBuilder.svg';
+import Logo from './assets/images/Logo_MHWildsArmorBuilder.svg';
+import Title from './assets/images/Title_MHWildsArmorBuilder.svg';
+import Icon_ArtifactLookup from '@/components/icons/Icon_ArtifactLookup.vue';
 
 import EquipmentCard from './components/EquipmentCard.vue';
 import FullBuildCard from './components/FullBuildCard.vue';
 
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, onBeforeUnmount } from 'vue';
+
+const equipmentCardImages = {
+  icon_artifactLookup: Icon_ArtifactLookup
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
+
+const showFab = ref(false);
+
+const handleScroll = () => {
+  showFab.value = window.scrollY > 100;
+};
 
 const equipmentPieces = ref([
   'Head',
@@ -115,9 +161,15 @@ const handleAllSkills = (allSkillsEmitted) => {
 </script>
 
 <style scoped>
-.appVue-vImg-logo{
-  height: 15vh;
-  width: auto;
+.appVue-vSlideXTransitionReverse-showFab {
+  position: fixed !important;
+  top: 16px;
+  right: 16px;
+  z-index: 9999;
+}
+
+.appVue-vSlideXTransitionReverse-showFab :deep(.v-icon){
+  transform: scale(1.4);
 }
 
 .appVue-vContainer {
